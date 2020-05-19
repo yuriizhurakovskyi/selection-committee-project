479350a4f3f38a6202ef43befb918e9e3a9f7bf6
Yurii Zhurakovskyi <yuriizhurakovskyi7@gmail.com>
2020-05-19 21:41:17 +0300

    changed name of applicantId variable

--- selection-committee/src/main/java/ua/lviv/yurii/zhurakovskyi/selectioncommittee/dao/CertificateRepository.java
+++ selection-committee/src/main/java/ua/lviv/yurii/zhurakovskyi/selectioncommittee/dao/CertificateRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ua.lviv.yurii.zhurakovskyi.selectioncommittee.domain.Certificate;
 
 public interface CertificateRepository extends JpaRepository<Certificate, Integer> {
-	Certificate findByApplicant(Integer id);
+	Certificate findByApplicant(Integer applicantid);
 }
