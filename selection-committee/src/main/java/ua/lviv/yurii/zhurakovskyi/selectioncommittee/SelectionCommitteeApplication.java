4a3c8aaaa3b544dd244348ee211ab806b463afbf
Yurii Zhurakovskyi <yuriizhurakovskyi7@gmail.com>
2020-05-19 10:47:37 +0300

    added an empty project

---
+++ selection-committee/src/main/java/ua/lviv/yurii/zhurakovskyi/selectioncommittee/SelectionCommitteeApplication.java
@@ -0,0 +1,13 @@
+package ua.lviv.yurii.zhurakovskyi.selectioncommittee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelectionCommitteeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelectionCommitteeApplication.class, args);
+	}
+
+}
---
+++ selection-committee/src/test/java/ua/lviv/yurii/zhurakovskyi/selectioncommittee/SelectionCommitteeApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.lviv.yurii.zhurakovskyi.selectioncommittee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelectionCommitteeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
